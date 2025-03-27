d95cacaee94e4987f77f0d94129978d0c9543595
Anjalikakade610k <71441240+Anjalikakade610k@users.noreply.github.com>
2025-03-27 10:48:44 -0700

    Task completion

--- app/components/articleDetailsCards/ArticleDetail.tsx
+++ app/components/articleDetailsCards/ArticleDetail.tsx
@@ -10,10 +10,17 @@ import { youngKidsArticleData } from "../../young-kids/api/getArticleforYoungkid
 import { Heart, HeartOutlined } from "../heart";
 import { middleSchoolArticleData } from "@/app/middle-school/api/getArticleforMiddleSchool";
 import { parentsArticleData } from "@/app/parents/api/getArticleData";
+import { Checkbox } from "@mui/material";
 
-function ArticleDetail({ article, category }: { article: IArticle; category: string }) {
+function ArticleDetail({
+  article,
+  category,
+}: {
+  article: IArticle;
+  category: string;
+}) {
   const [isFavoriteState, setIsFavoriteState] = useState(article.isFavorite);
-
+  const router = useRouter();
   const toggleFavorite = () => {
     setIsFavoriteState(!isFavoriteState);
   };
@@ -32,7 +39,9 @@ function ArticleDetail({ article, category }: { article: IArticle; category: str
     }
   };
 
-  const currentArticleDetail = getCategoryData(category).find((a) => a.name === article.name);
+  const currentArticleDetail = getCategoryData(category).find(
+    (a) => a.name === article.name
+  );
 
   // Fetch all articles from the same source (excluding the current one)
   const relatedArticles = getCategoryData(category).filter(
@@ -43,48 +52,80 @@ function ArticleDetail({ article, category }: { article: IArticle; category: str
     <div className="article-container">
       {/* Header */}
       <div className={styles.header}>
-        <Image className={styles.backButton} src={backIcon} alt="Back Icon" />
+        <button className={styles.buttn} onClick={() => router.back()}>
+          <Image className={styles.backButton} src={backIcon} alt="Back Icon" />
+        </button>
         <span className={styles.headerTitle}>Articles</span>
       </div>
 
       {/* Article Details */}
       <div className={`${styles.card} flex-row`}>
         <div className={`image ${styles.cardImg}`}>
-          <Image src={currentArticleDetail?.img} fill={true} alt={article.name} />
+          <Image
+            src={currentArticleDetail?.img}
+            fill={true}
+            alt={article.name}
+          />
         </div>
         <div className={`${styles.cardContent} flex-column`}>
           <div className={`${styles.carddetails} flex-column`}>
             <span className={styles.subtitle}>{article.source.name}</span>
             <span className={styles.title}>{article.name}</span>
             <span className={styles.description}>{article.description}</span>
-            <button className={styles.exploreButton}>Explore More →</button>
+            
           </div>
-          <div className={`${styles.cardtoggles} flex-column`}>
-            {!isFavoriteState ? (
-              <HeartOutlined className={styles.heartButton} onClick={toggleFavorite} />
-            ) : (
-              <Heart className={styles.heartButton} onClick={toggleFavorite} />
-            )}
+          <button className={styles.exploreButton}>Explore More →</button>
+          <div className={`${styles.cardtoggles} flex-row`}>
+            <div className={`${styles.checkboxStyling} flex-row`}>
+              <Checkbox />
+              <span className={styles.label}>Finished Reading</span>{" "}
+            </div>
+            <div className={`${styles.heartStyling} flex-row`}>
+              {!isFavoriteState ? (
+                <HeartOutlined
+                  className={styles.heartButton}
+                  onClick={toggleFavorite}
+                />
+              ) : (
+                <Heart
+                  className={styles.heartButton}
+                  onClick={toggleFavorite}
+                />
+              )}
+              <span className={styles.label}>Like</span>{" "}
+            </div>
           </div>
         </div>
       </div>
 
       {/* From Creators Section */}
-      <div className={styles.fromCreatorsSection}>
-        <h1>From Creators</h1>
-        <div className={styles.relatedArticles}>
-          {relatedArticles.map((relatedArticle) => (
-            <div key={relatedArticle.name} className={styles.relatedArticleCard}>
-              <Image
-                src={relatedArticle.img}
-                alt={relatedArticle.name}
-                width={100}
-                height={100}
-                className={styles.relatedArticleImage}
-              />
-              <span className={styles.relatedArticleTitle}>{relatedArticle.name}</span>
-            </div>
-          ))}
+
+      <h1 className={`${styles.fromcreatortitle}`}>From Creator</h1>
+      <div className={`${styles.fromCreatorsSection} `}>
+        <div className={`${styles.relatedArticles} flex-row`}>
+          {relatedArticles?.length > 0 ? (
+            relatedArticles.map((relatedArticle) => (
+              <div
+                key={relatedArticle.name}
+                className={styles.relatedArticleCard}
+              >
+                <div className={`${styles.relatedArticleImage}`}>
+                  <Image
+                    src={relatedArticle.img}
+                    alt={relatedArticle.name}
+                    width={260}
+                    height={240}
+                    className={styles.ArticleImage}
+                  />
+                </div>
+                <span className={styles.relatedArticleTitle}>
+                  {relatedArticle.name}
+                </span>
+              </div>
+            ))
+          ) : (
+            <p>No related articles available.</p>
+          )}
         </div>
       </div>
     </div>
